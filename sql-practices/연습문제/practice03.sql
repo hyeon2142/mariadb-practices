ddfbc9877225605b6f5e2d6d58a261bafef296b4
hyeon2142 <hyeon2142@naver.com>
2021-10-06 09:34:14 +0900

    practice 1~3 resolved

--- sql-practices/연습문제/practice03.sql
+++ sql-practices/연습문제/practice03.sql
@@ -28,22 +28,48 @@ from employees e,salaries s,titles t,dept_emp d
 where e.emp_no = s.emp_no and e.emp_no = t.emp_no and e.emp_no = d.emp_no
 order by concat(first_name," ",last_name) desc;
 
-
-
 /*문제5.
 ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. (현재 ‘Technique Leader’의 직책(으로 근무하는 사원은 고려하지 않습니다.) 이름은 first_name과 last_name을 합쳐 출력 합니다.*/
+select e.emp_no,concat(first_name,' ',last_name) as name
+from employees e,titles t
+where e.emp_no = t.emp_no and title = 'Technique Leader' and from_date != '9999-01-01';
+
 
 /*문제6.
 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.*/
+select a.emp_no, concat(a.first_name, ' ', a.last_name), c.dept_name, d.title
+from employees a, dept_emp b, departments c, titles d
+where a.emp_no = b.emp_no and b.dept_no = c.dept_no and a.emp_no = d.emp_no and a.last_name like 'S%';
 
 /*문제7.
 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.*/
+select e.emp_no,concat(first_name, ' ', last_name) as name,salary,title
+from employees e,titles t,salaries s
+where e.emp_no = t.emp_no and e.emp_no = s.emp_no and s.to_date = '9999-01-01' and title = 'Engineer' and salary >= 40000
+order by salary desc;
 
 /*문제8.
 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오*/
+select e.emp_no, t.title, s.salary
+from employees e, salaries s, titles t
+where e.emp_no = t.emp_no and e.emp_no = s.emp_no and s.to_date = '9999-01-01' and t.to_date - '9999-01-01' and s.salary > 50000
+ order by s.salary desc;
 
 /*문제9.
 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.*/
+select d.dept_name, avg(salary)
+from employees a, dept_emp b, salaries c, departments d
+where a.emp_no = b.emp_no and a.emp_no = c.emp_no and b.dept_no = d.dept_no and b.to_date = '9999-01-01' and c.to_date = '9999-01-01'
+group by d.dept_name
+order by avg(salary) desc;
 
 /*문제10.
 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.*/
+select c.title, avg(b.salary)
+from employees a, salaries b, titles c
+where a.emp_no = b.emp_no
+      and a.emp_no = c.emp_no
+      and b.to_date = '9999-01-01'
+      and c.to_date - '9999-01-01'
+ group by c.title
+ order by avg(b.salary) desc;
